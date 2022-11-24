0bd5c84707306269d6558c343a7a269c69c14d28
Daniel Stephen Ruiz <118859674+EseRuiz@users.noreply.github.com>
2022-11-24 01:21:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#dado un numero entero x y 'y' calcular x**y y mostrar resultado
+x = int(input("Ingrese numero entero x: "))
+y = int(input("Ingrese numero entero y: "))
+
+print(f"El resultado es: {x**y}")
